d68b3737a765497cb00fa91afffa79812d9b1742
rza35 <riza_tire@hotmail.com>
2021-03-17 21:06:29 +0300

    angi

--- src/app/components/carimage/carimage.component.ts
+++ src/app/components/carimage/carimage.component.ts
@@ -5,7 +5,7 @@ import { CarimageService } from 'src/app/services/carimage.service';
 @Component({
   selector: 'app-carimage',
   templateUrl: './carimage.component.html',
-  styleUrls: ['./carimage.component.css']
+  styleUrls: ['./carimage.component.css'],
 })
 export class CarimageComponent implements OnInit {
 
